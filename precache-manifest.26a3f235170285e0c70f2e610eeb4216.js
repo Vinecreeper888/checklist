aafd7d75ca13da45d6bd804fb8845abb0d405315
Vinecreeper888 <siddhanth8@gmail.com>
2020-12-28 17:11:03 +0530

    Updates

---
+++ precache-manifest.26a3f235170285e0c70f2e610eeb4216.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95297192176a4a79269e40f66ad2e307",
+    "url": "/checklist/index.html"
+  },
+  {
+    "revision": "df0b77208db7bff78cc9",
+    "url": "/checklist/static/js/2.cacf128c.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/checklist/static/js/2.cacf128c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b6e21b2f903f45207d2",
+    "url": "/checklist/static/js/main.418e4451.chunk.js"
+  },
+  {
+    "revision": "807e25d3e0d7bd4896dc",
+    "url": "/checklist/static/js/runtime-main.f07b4aa4.js"
+  }
+]);
